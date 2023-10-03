0f2f06624e41b0daff2c4ede8fb846124616f464
Maaajaaa <maaajaaa@riseup.net>
2023-10-03 19:02:11 +0200

    Revert "logger changes for future reference only"
    This reverts commit 1c61cc01fa8f2dd5166b525c349c0fb9ce764bed.

--- src/Logger/Logger.h
+++ src/Logger/Logger.h
@@ -1,20 +1,19 @@
 #include "FS.h"
 #include "SPIFFS.h"
 #include "BParasite.h"
-#include <ctime>
-#include <config.h>
 
 /* You only need to format SPIFFS the first time you run a
    test or else use the SPIFFS plugin to create a partition
    https://github.com/me-no-dev/arduino-esp32fs-plugin */
 #define FORMAT_SPIFFS_IF_FAILED true
 
+#define mstLogFile0 "/moistLog0.csv"
+
 class Logger{
     public:
         Logger();
         bool begin();
-        bool logData(int sensor_id, BParasite_Data_S data, time_t time);
-        std::vector<std::string> getLogFilePaths();
+        bool logData(int data_id, std::string data);
     private:
         void listDir(fs::FS &fs, const char * dirname, uint8_t levels);
         void readFile(fs::FS &fs, const char * path);
@@ -22,5 +21,4 @@ class Logger{
         void appendFile(fs::FS &fs, const char * path, const char * message);
 
         bool successfulStart;
-        std::vector<std::string> logFileNames;
 };
