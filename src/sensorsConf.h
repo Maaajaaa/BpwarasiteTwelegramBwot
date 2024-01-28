b63afda9aed5020c9c8b4e8579d664fa39d96d6f
Maaajaaa <maaajaaa@riseup.net>
2024-01-28 17:06:53 +0100

    fixed wood storage ivy's MAC

--- src/sensorsConf.h
+++ src/sensorsConf.h
@@ -4,7 +4,7 @@
 
 #ifdef MAJA_conf
     std::vector<std::string> plantNames {"avocados", "Maja's ivy", "terrarium ivy", "avocados mirror","window ivy","wood storage ivy"};
-    std::vector<std::string> knownBLEAddresses {"EF:59:22:76:F7:EC", "D4:8C:FB:66:EA:17", "E2:37:24:4E:DA:FA", "E1:05:13:31:69:FC","C7:7A:B5:4C:C8:4D","E1:05:13:31:69:FC"};
+    std::vector<std::string> knownBLEAddresses {"EF:59:22:76:F7:EC", "D4:8C:FB:66:EA:17", "E2:37:24:4E:DA:FA", "E1:05:13:31:69:FC","C7:7A:B5:4C:C8:4D","F6:01:BE:1C:7E:70"};
 #endif
 
 
